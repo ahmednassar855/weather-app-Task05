7be513cd82da8941bf83a4e0eba52934e681f9e4
ahmed nassar <ahmed.nassar855@gmail.com>
2022-10-20 20:11:47 +0200

    eighth Commit

--- js/main.js
+++ js/main.js
@@ -15,134 +15,174 @@ function locationNameValidation(){
   var locationNameRegex = /^[a-z]{3,20}$/ig; 
   if (sercheadLocation.value.match(locationNameRegex) ){
     return true;
-  }else if (sercheadLocation.value = ""){
-    sercheadLocation.value = "cairo";
   }
   else{
-   errorMessage()
+   errorMessage();
    console.log("please enter character only min 3 max 20 chaarcters");
    return false;
   } 
 }
 
-function search(){
-  if (locationNameValidation()){
-    var getVaildLocation = sercheadLocation.value.toLowerCase();
-    var req = new XMLHttpRequest();
-    req.open("GET", `https://api.weatherapi.com/v1/forecast.json?key=72c1c3226ade4eecbb4135111221610&q=${getVaildLocation}&days=7`);
-    req.send();
-    
-    req.addEventListener("loadend" , function(){
-      if (req.status == 400){
-        console.log("this value is not exist please try again")
+
+
+async function cairo() {
+   var reqApi = await fetch('https://api.weatherapi.com/v1/forecast.json?key=72c1c3226ade4eecbb4135111221610&q=cairo&days=7');
+   var apiResult = await reqApi.json();
+   if (allForecasts.length  >= 0){
+      allForecasts.splice(allForecasts[0],1);
+   }
+   allForecasts.push(apiResult);
+   console.log(allForecasts);
+   display();
+};
+cairo();
+
+
+async function search(){
+   var x = sercheadLocation.value;
+   if ( locationNameValidation() == true){
+      var reqApi = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=72c1c3226ade4eecbb4135111221610&q=${x}&days=7`);
+      
+         var apiResult = await reqApi.json();
+         if(apiResult != null){
+            if (allForecasts.length  >= 0){
+                  allForecasts.splice(allForecasts[0],1);
+               }
+            allForecasts.push(apiResult);
+            console.log(x, allForecasts);
+            display();
+         }else{
+            errorMessage();
       }
-      if ( req.status == 200 ){
-          var getResponse = JSON.parse( req.response );
-          if (allForecasts.length  >= 0){
-            allForecasts.splice(allForecasts[0],1);
-         }
-          allForecasts.push(getResponse);
-          
-          console.log("hamada")
-          console.log(allForecasts);
-         display();
-      }  
-  });
-  }
-  
+      
+   }
 }
+
+
+
+// function search(){
+//   if (locationNameValidation()){
+//     var getVaildLocation = sercheadLocation.value.toLowerCase();
+//     var req = new XMLHttpRequest();
+//     req.open("GET", `https://api.weatherapi.com/v1/forecast.json?key=72c1c3226ade4eecbb4135111221610&q=${getVaildLocation}&days=7`);
+//     req.send();
+    
+//     req.addEventListener("loadend" , function(){
+//       if (req.status == 400){
+//         console.log("this value is not exist please try again")
+//       }
+//       if ( req.status == 200 ){
+//           var getResponse = JSON.parse( req.response );
+//           if (allForecasts.length  >= 0){
+//             allForecasts.splice(allForecasts[0],1);
+//          }
+//           allForecasts.push(getResponse);
+          
+//           console.log("hamada")
+//           console.log(allForecasts);
+//          display();
+//       }  
+//   });
+//   }
+  
+// }
+
 
 // function to get the weedend day name  of current day
 function getWeekendDayName(){
-  var timestamp = allForecasts[0].forecast.forecastday[0].date_epoch;
-  var xx = new Date();
-  var yy = xx.setTime(timestamp*1000);
-  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  var day = days[xx.getDay()];
-  return day;
+   var timestamp = allForecasts[0].forecast.forecastday[0].date_epoch;
+   var xx = new Date();
+   var yy = xx.setTime(timestamp*1000);
+   var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   var day = days[xx.getDay()];
+   return day;
 }
 
 // function to get the weedend day name after one day
 function getWeekendNextDayName(){
-  var timestamp = allForecasts[0].forecast.forecastday[1].date_epoch;
-  var xx = new Date();
-  var yy = xx.setTime(timestamp*1000);
-  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  var day = days[xx.getDay()];
-  return day;
+   var timestamp = allForecasts[0].forecast.forecastday[1].date_epoch;
+   var xx = new Date();
+   var yy = xx.setTime(timestamp*1000);
+   var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   var day = days[xx.getDay()];
+   return day;
 }
 
 // function to get the weedend day name  after two days
 function getWeekendNextAfterDayName(){
-  var timestamp = allForecasts[0].forecast.forecastday[2].date_epoch;
-  var xx = new Date();
-  var yy = xx.setTime(timestamp*1000);
-  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  var day = days[xx.getDay()];
-  return day;
+   var timestamp = allForecasts[0].forecast.forecastday[2].date_epoch;
+   var xx = new Date();
+   var yy = xx.setTime(timestamp*1000);
+   var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   var day = days[xx.getDay()];
+   return day;
 }
 
-// funvtion to get the wind direction by name
+// function to get the wind direction by name
 var windDirection = "";
 function determinWindDirection(){
-  if ( allForecasts[0].current.wind_dir == "W" ){
-     windDirection = 'West';
-  }
-  if ( allForecasts[0].current.wind_dir == "N"){
-     windDirection = "North"
-  }
-  if ( allForecasts[0].current.wind_dir == "E"){
-     windDirection = "East"
-  }
-  if ( allForecasts[0].current.wind_dir == "S"){
-     windDirection = "South"
-  }
-  if ( allForecasts[0].current.wind_dir == "NE"){
-     windDirection = "North-East"
-  }
-  if ( allForecasts[0].current.wind_dir == "SE"){
-     windDirection = "South-East"
-  }
-  if ( allForecasts[0].current.wind_dir == "SW"){
-     windDirection = "South-East"
-  }
-  if ( allForecasts[0].current.wind_dir == "NW"){
-     windDirection = "North-East";
-  }
-  if ( allForecasts[0].current.wind_dir == "NNE"){
-     windDirection = 'North North-East';
-  }
-  if ( allForecasts[0].current.wind_dir == "NSE"){
-     windDirection = "North South-East";
-  }
-  if ( allForecasts[0].current.wind_dir == "ENW"){
-     windDirection = "East North-west";
-  }
-  if ( allForecasts[0].current.wind_dir == "ESW"){
-     windDirection = "East south-west";
-  }
-  if ( allForecasts[0].current.wind_dir == "SSE"){
-     windDirection = "South South-East"
-  }
-  if ( allForecasts[0].current.wind_dir == "SSW"){
-     windDirection = "South south west";
-  }
-  if ( allForecasts[0].current.wind_dir == "WSW"){
-     windDirection = "west south-west"
-  }
-  if ( allForecasts[0].current.wind_dir == "WNW"){
-     windDirection = "west north-west"
-  }
-  console.log(windDirection);
+   if ( allForecasts[0].current.wind_dir == "W" ){
+      windDirection = 'West';
+   }
+   if ( allForecasts[0].current.wind_dir == "N"){
+      windDirection = "North"
+   }
+   if ( allForecasts[0].current.wind_dir == "E"){
+      windDirection = "East"
+   }
+   if ( allForecasts[0].current.wind_dir == "S"){
+      windDirection = "South"
+   }
+   if ( allForecasts[0].current.wind_dir == "NE"){
+      windDirection = "North-East"
+   }
+   if ( allForecasts[0].current.wind_dir == "SE"){
+      windDirection = "South-East"
+   }
+   if ( allForecasts[0].current.wind_dir == "SW"){
+      windDirection = "South-East"
+   }
+   if ( allForecasts[0].current.wind_dir == "NW"){
+      windDirection = "North-East";
+   }
+   if ( allForecasts[0].current.wind_dir == "NNE"){
+      windDirection = 'North North-East';
+   }
+   if ( allForecasts[0].current.wind_dir == "NSE"){
+      windDirection = "North South-East";
+   }
+   if ( allForecasts[0].current.wind_dir == "ENW"){
+      windDirection = "East North-west";
+   }
+   if ( allForecasts[0].current.wind_dir == "ESW"){
+      windDirection = "East south-west";
+   }
+   if ( allForecasts[0].current.wind_dir == "SSE"){
+      windDirection = "South South-East"
+   }
+   if ( allForecasts[0].current.wind_dir == "SSW"){
+      windDirection = "South south west";
+   }
+   if ( allForecasts[0].current.wind_dir == "WSW"){
+      windDirection = "west south-west"
+   }
+   if ( allForecasts[0].current.wind_dir == "WNW"){
+      windDirection = "west north-west"
+   }else{
+      windDirection = "Normal";
+   }
+   console.log(windDirection);
 }
 
 function errorMessage(){
       document.querySelector('.error-message').classList.replace('d-none','d-flex');  
 } 
 
-function display(){
+async function display(){
    // got data and displayed in first column
   //  changeTempDegree();
+   // got data and displayed in first column
+   //  changeTempDegree();
    document.querySelector('.error-message').classList.replace('d-flex','d-none');
 
    determinWindDirection();
